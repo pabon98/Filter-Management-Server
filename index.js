264b7f588109992e7ab1245db275062fe4da07fb
pabon98 <hasinanjumpabon@gmail.com>
2022-08-17 23:46:19 +0600

    all methods added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ async function run(){
         await client.connect()
         const database = client.db('filtersdb')
         const filtersCollection = database.collection('filters')
+        const favouritefilterCollection = database.collection('favouritefilters')
 
         //GET API for geting filters item from db
         app.get('/filters', async (req,res)=>{
@@ -27,12 +29,64 @@ async function run(){
             res.send(filters)
         })
 
+        app.get('/filtermanagement/:id', async (req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const filter = await favouritefilterCollection.findOne(query)
+            // console.log('load users with id', id);
+            res.send(filter)
+        })
+
+
         //POST API for posting filters into db
         app.post('/filters', async (req,res)=>{
             const newFilter = req.body
             const result = await filtersCollection.insertOne(newFilter)
             // console.log('new filter added', req.body)
             res.json(result)
+        })
+
+        //POST API for posting favourite filters into db
+        app.post('/favouritefilters', async(req,res)=>{
+            const newFabouriteFilter = req.body
+            const result = await favouritefilterCollection.insertOne(newFabouriteFilter)
+            res.json(result)
+        })
+        //GET API for geting favourite filters item from db
+        app.get('/favouritefilters', async (req,res)=>{
+            const cursor = favouritefilterCollection.find({})
+            const favouritefilters = await cursor.toArray()
+            res.send(favouritefilters)
+        })
+
+        //UPDATE API
+        app.put('/filtermanagement/:id', async(req,res)=>{
+            const id = req.params.id
+            const updatedFilter = req.body
+            // console.log(updatedFilter);
+            const filter = {_id: ObjectId(id)}
+            // console.log(filter)
+            const options = {upsert: true}
+            const updateDoc = {
+                $set: {
+                  title: updatedFilter.title,
+                  request: updatedFilter.request,
+                  transportation: updatedFilter.transportation,
+                  cities: updatedFilter.cities
+                },
+              };
+              const result = await favouritefilterCollection.updateOne(filter, updateDoc, options)
+            //  console.log('updating user', id)
+             res.json(result)
+        })
+
+        //DELETE API for deleting filter
+        app.delete('/favouritefilters/:id', async(req,res)=>{
+         const id = req.params.id
+         const query ={_id: ObjectId(id)}
+         const result = await favouritefilterCollection.deleteOne(query)
+        //  console.log('deleting filters id', id)
+         res.json(result)
         })
     }
     finally {
@@ -42,7 +96,7 @@ async function run(){
 run().catch(console.dir)
 
 app.get("/", (req, res) => {
-    res.send("running filter server");
+    res.send("running my filter server");
   });
   
   app.listen(port, () => {
